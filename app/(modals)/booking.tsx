a1b38347937c034e83ad206917f9438bd5c71d51
Tomasz Prochowicz <tprochowicz@MacBook-Pro-Tomasz.local>
2024-08-04 23:25:54 +0200

    feat: add modals screen

---
+++ app/(modals)/booking.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Page = () => {
+  return <Text>Book</Text>;
+};
+
+export default Page;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,9 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import { TouchableOpacity } from "react-native";
 import "react-native-reanimated";
 
 export {
@@ -49,9 +45,39 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
+  const router = useRouter();
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modals)/login"
+        options={{
+          title: "Login in or sign up",
+          headerTitleStyle: {
+            fontFamily: "mon-sb",
+          },
+          presentation: "modal",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen name="listing/[id]" options={{ headerTitle: "" }} />
+      <Stack.Screen
+        name="(modals)/booking"
+        options={{
+          presentation: "transparentModal",
+          animation: "fade",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </Stack>
   );
 }
